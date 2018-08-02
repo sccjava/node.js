982e21e9fbd8b0fb48f112b3a85c12186cdf53d3
Sunny Sun <Sunny.Sun3@harman.com>
2018-08-02 16:20:04 +0800

    add image matching script

---
+++ android_res_format_hkremote.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " path/to/directory");
+    process.exit(-1);
+}
+
+var path = process.argv[2];
+
+var filenameMap = new Map();
+filenameMap.set('ok_press.png', 'ok_pressed.png');
+filenameMap.set('back_press.png', 'back_pressed.png');
+filenameMap.set('demo_press.png', 'demo_pressed.png');
+filenameMap.set('info_press.png', 'info_pressed.png');
+filenameMap.set('menu_press.png', 'menu_pressed.png');
+filenameMap.set('options_press.png', 'options_pressed.png');
+filenameMap.set('tone_press.png', 'tone_pressed.png');
+filenameMap.set('sourceAPress.png', 'source_a_pressed.png');
+filenameMap.set('sourceBPress.png', 'source_b_pressed.png');
+filenameMap.set('sourceCPress.png', 'source_c_pressed.png');
+filenameMap.set('sourceDPress.png', 'source_d_pressed.png');
+filenameMap.set('pad_bds_rc_volume_down.png', 'pad_bds_rc_volum_down.png');
+filenameMap.set('pad_bds_rc_volume_down_press.png', 'pad_bds_rc_volum_down_press.png');
+filenameMap.set('pad_bds_rc_volume_up.png', 'pad_bds_rc_volum_up.png');
+filenameMap.set('pad_bds_rc_volume_up_press.png', 'pad_bds_rc_volum_up_press.png');
+
+function renameFile2(path) {
+    fs.readdir(path, function (err, items) {
+        if (err) throw err;
+        for (var i = 0; i < items.length; i++) {
+            if (items[i] == ".DS_Store")
+                continue;
+            var oldPath = path + '/' + items[i];
+            if (fs.lstatSync(oldPath).isDirectory()) {
+                renameFile2(oldPath);
+            } else {
+                if (filenameMap.get(items[i])) {
+                    var newFile = path + '/' + filenameMap.get(items[i]);
+                    console.log(oldPath + " -----rename-------> " + newFile);
+                    fs.renameSync(oldPath, newFile);
+                    continue;
+                }
+
+                let reg = /([A-Z]*)/g;
+                let match = items[i].match(reg);
+                if (match && match[0]) {
+                    var newFile = path + '/' + items[i].replace(match[0], '_' + match[0].toLowerCase());
+                    console.log(oldPath + " ------------> " + newFile);
+                    fs.renameSync(oldPath, newFile);
+                }
+            }
+        }
+    });
+}
+
+renameFile2(path);
+
+
---
+++ match_images.js
@@ -0,0 +1,95 @@
+/*
+1. UI team has changed new font for icons.
+2. Need to replace the icons(download from invisionapp) to exist project.
+3. So use this script to match icons based on filename & resolution.
+ */
+var path = require('path');
+var fs = require('fs');
+var sizeOf = require('image-size'); // npm install image-size
+var rimraf = require('rimraf'); // npm install rimraf
+
+console.log('argv length=' + process.argv.length);
+
+if (process.argv.length < 4) {
+    console.log("Usage: " + __filename + " existIconsPath newIconsPath");
+    process.exit(-1);
+}
+
+var existIconsPath = process.argv[2];
+var newIconsPath = process.argv[3];
+
+var matchFilesMap = new Map();
+var notMatchFilesMap = new Map();
+
+var matchIconsOut = existIconsPath + '_new';
+console.log('Match icons will copy to ' + matchIconsOut);
+rimraf.sync(matchIconsOut);
+fs.mkdirSync(matchIconsOut);
+
+
+fs.readdir(existIconsPath, function (err, items) {
+    if (err) throw err;
+    var totalFileCnt = 0;
+    var fileNotFound = 0;
+
+    for (var i = 0; i < items.length; i++) {
+        var fullpath = existIconsPath + "/" + items[i];
+        if (fs.lstatSync(fullpath).isFile() && items[i].endsWith('.png')) {
+            totalFileCnt++;
+
+            var oldDimension = sizeOf(fullpath);
+            var isFound = false;
+
+            findFile(newIconsPath, items[i], function (filePath) {
+                if (path.basename(fullpath) != path.basename(filePath)) {
+                    console.log('file name not match ', fullpath, filePath);
+                    return;
+                }
+                isFound = true;
+                var newDimension = sizeOf(filePath);
+                var fileName = path.basename(filePath);
+                if (Math.abs(oldDimension.width - newDimension.width) <= 1 && Math.abs(oldDimension.height - newDimension.height) <= 1) {
+                    matchFilesMap.set(fileName, oldDimension.width + ',' + oldDimension.height);
+                    fs.copyFileSync(filePath, matchIconsOut + '/' + fileName);
+                } else {
+                    notMatchFilesMap.set(fileName, oldDimension.width + ',' + oldDimension.height);
+                }
+            });
+            if (!isFound) {
+                fileNotFound++;
+                console.log('Not found ', fullpath);
+            }
+        }
+    }
+
+    for (var [key, value] of matchFilesMap) {
+        notMatchFilesMap.delete(key);
+    }
+    console.log('Not match ', notMatchFilesMap);
+
+    console.log('totalFileCnt ', totalFileCnt, ',dimensionMatchCnt ', matchFilesMap.size, ',dimensionNotMatchCnt ', notMatchFilesMap.size, ', fileNotFound ', fileNotFound);
+});
+
+
+function findFile(startPath, filter, callback) {
+    if (!fs.existsSync(startPath)) {
+        console.log("no dir ", startPath);
+        return;
+    }
+
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            findFile(filename, filter, callback); //recurse
+        }
+        else if (filter == files[i]) callback(filename);
+    }
+};
+
+// Milliseconds
+function sleep_ms(millisecs) {
+    var initiation = new Date().getTime();
+    while ((new Date().getTime() - initiation) < millisecs) ;
+}
